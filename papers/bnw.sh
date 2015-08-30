d82cfb2217bad1032ef5ff0c8b4899707a3036d7
tungufoss <helga85@gmail.com>
2015-08-30 12:00:41 +0000

    Convert pdf to grayscale

---
+++ papers/bnw.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+gs \
+ -sOutputFile=output.pdf \
+ -sDEVICE=pdfwrite \
+ -sColorConversionStrategy=Gray \
+ -dProcessColorModel=/DeviceGray \
+ -dCompatibilityLevel=1.4 \
+ -dNOPAUSE \
+ -dBATCH \
+ $1
